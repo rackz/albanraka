6348c62cf01ef6e82ebed0f1ca93f656598d758c
325102069 <325102069@C-H63W3V1.tdsb.on.ca>
2017-01-12 13:23:49 -0500

    Completed Mississippi

---
+++ ICS3/src/raka/Mississippi.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package raka;
+
+/**
+ *Alban Raka
+ *12/01/17
+ *This program prints out the word Mississippi
+ *ICS3U
+ */
+public class Mississippi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+		printM();
+		printI();
+		printS();
+		printS();
+		printI();
+		printS();
+		printS();
+		printI();
+		printP();
+		printP();
+		printI();
+	}
+   
+	/**
+	 * draws the letter M
+	 */
+	public static void printM(){
+    	
+       System.out.println("M     M"); 
+       System.out.println("MM   MM");
+       System.out.println("M M M M");
+       System.out.println("M  M  M");
+       System.out.println("M     M");
+       System.out.println("M     M");
+       System.out.println("M     M");
+    }
+    
+	/**
+	 * draws the letter I
+	 */
+    public static void printI(){
+    	System.out.println("IIIII");
+    	System.out.println("  I  ");
+    	System.out.println("  I  ");
+    	System.out.println("  I  ");
+    	System.out.println("  I  ");
+    	System.out.println("  I  ");
+    	System.out.println("IIIII");
+    }
+    	
+    /**
+	 * draws the letter S
+	 */
+    public static void printS(){
+     	System.out.println(" SSSSS ");
+     	System.out.println("S     S");
+     	System.out.println("S");
+     	System.out.println(" SSSSS ");
+     	System.out.println("      S");
+     	System.out.println("S      S");
+     	System.out.println(" SSSSS ");
+    }
+    	
+    /**
+	 * draws the letter P
+	 */
+    public static void printP(){
+    	System.out.println(" PPPPP ");
+    	System.out.println("P     P");
+    	System.out.println("P     P");
+    	System.out.println("P P P  ");
+    	System.out.println("P      ");
+    	System.out.println("p      ");
+    	System.out.println("P      ");
+       
+       
+	}
+
+}
